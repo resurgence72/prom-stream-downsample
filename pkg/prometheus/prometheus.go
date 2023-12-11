eb38de35d85978942932ea86a40e7af3e63b12fb
Resurgence72 <wuhaoyang1105@qq.com>
2023-12-11 18:11:27 +0800

    Update prometheus.go

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -1,21 +1,30 @@
 package prometheus
 
 import (
+	"fmt"
 	"net/url"
+	"strings"
+	"time"
+
+	"github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/storage/remote"
 
 	"prom-stream-downsample/pkg/pb"
 
 	"github.com/prometheus/prometheus/prompb"
-	"golang.org/x/mod/semver"
 )
 
 const initialBufSize = 32 * 1024
 
 type Prometheus struct {
-	remoteReadURL  string
-	remoteWriteURL string
-	enabledStream  bool
-	remoteReadType string
+	remoteReadGroup []string
+	remoteWriteURL  string
+	enabledStream   bool
+	remoteReadType  string
+
+	queryables []storage.SampleAndChunkQueryable
 
 	writeCh chan []prompb.TimeSeries
 }
@@ -32,16 +41,16 @@ func (p Prometheus) RemoteReadType() string {
 }
 
 func NewPrometheus(
-	rr string,
+	rrg []string,
 	rw string,
 	enabled bool,
 	writeCh chan []prompb.TimeSeries,
 ) (*Prometheus, error) {
 	p8s := &Prometheus{
-		remoteReadURL:  rr,
-		remoteWriteURL: rw,
-		enabledStream:  enabled,
-		writeCh:        writeCh,
+		remoteReadGroup: rrg,
+		remoteWriteURL:  rw,
+		enabledStream:   enabled,
+		writeCh:         writeCh,
 	}
 	supported, err := p8s.versionSupportStreamRemoteRead()
 	if err != nil {
@@ -52,20 +61,50 @@ func NewPrometheus(
 	if !p8s.enabledStream && supported {
 		p8s.enabledStream = true
 	}
+
 	p8s.remoteReadType = pb.RemoteReadType[p8s.enabledStream]
+
+	var queryables []storage.SampleAndChunkQueryable
+	for i, rr := range rrg {
+		u, _ := url.Parse(rr)
+		rc, err := remote.NewReadClient(fmt.Sprintf("remote-%d", i), &remote.ClientConfig{
+			URL:     &config.URL{u},
+			Timeout: model.Duration(30 * time.Second),
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		queryables = append(queryables, remote.NewSampleAndChunkQueryableClient(
+			rc,
+			nil,
+			nil,
+			true,
+			nil,
+		))
+	}
+
+	p8s.queryables = queryables
 	return p8s, nil
 }
 
 func (p *Prometheus) versionSupportStreamRemoteRead() (bool, error) {
-	u, _ := url.Parse(p.remoteReadURL)
-	info, err := NewPrometheusMetaInfo(u.Scheme + "://" + u.Host + "/")
-	if err != nil {
-		return false, err
+	for _, rr := range p.remoteReadGroup {
+		u, err := url.Parse(rr)
+		if err != nil {
+			return false, err
+		}
+
+		info, err := NewPrometheusMetaInfo(u.Scheme + "://" + u.Host + "/")
+		if err != nil {
+			return false, err
+		}
+
+		// 如果当前版本号大于2.13.0，则开启自动流式传输
+		if strings.Compare(info.Version, "2.13.0") <= 0 {
+			return false, nil
+		}
 	}
 
-	// 如果当前版本号大于2.13.0，则开启自动流式传输
-	if semver.Compare(semver.Build(info.Version), "2.13.0") >= 0 {
-		return true, nil
-	}
-	return false, nil
+	return true, nil
 }
