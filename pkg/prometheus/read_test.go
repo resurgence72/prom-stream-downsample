1e77de1ed7e404ecc2a2efe27e8c5c737b8a54d5
Resurgence72 <wuhaoyang1105@qq.com>
2023-12-11 15:39:50 +0800

    Update read_test.go

--- pkg/prometheus/read_test.go
+++ pkg/prometheus/read_test.go
@@ -58,29 +58,73 @@ func TestRemoteRead(t *testing.T) {
 	ctx := context.Background()
 	maxt := time.Now()
 	mint := maxt.Add(-1 * time.Minute)
-	queriers := make([]storage.Querier, 0, len(queryables))
+
+	// sample query
+	//queriers := make([]storage.Querier, 0, len(queryables))
+	//for _, queryable := range queryables {
+	//	q, err := queryable.Querier(ctx, mint.UnixMilli(), maxt.UnixMilli())
+	//	if err != nil {
+	//		t.Fatal(err)
+	//	}
+	//	queriers = append(queriers, q)
+	//}
+
+	//ss := storage.NewMergeQuerier(nil, queriers, storage.ChainedSeriesMerge).Select(
+	//	false,
+	//	nil,
+	//	labels.MustNewMatcher(labels.MatchEqual, "__name__", "up"),
+	//)
+	//
+	//for ss.Next() {
+	//	series := ss.At()
+	//	lbs := series.Labels()
+	//	it := series.Iterator(nil)
+	//	for it.Next() == chunkenc.ValFloat {
+	//		ts, val := it.At()
+	//		fmt.Printf("%s %g %d\n", lbs, val, ts)
+	//	}
+	//	if it.Err() != nil {
+	//		t.Fatal(err)
+	//	}
+	//}
+
+	// stream query
+
+	queriers := make([]storage.ChunkQuerier, 0, len(queryables))
 	for _, queryable := range queryables {
-		q, err := queryable.Querier(ctx, mint.UnixMilli(), maxt.UnixMilli())
+		q, err := queryable.ChunkQuerier(ctx, mint.UnixMilli(), maxt.UnixMilli())
 		if err != nil {
 			t.Fatal(err)
 		}
 		queriers = append(queriers, q)
 	}
-
-	ss := storage.NewMergeQuerier(nil, queriers, storage.ChainedSeriesMerge).Select(
+	ss := storage.NewMergeChunkQuerier(nil, queriers, storage.NewConcatenatingChunkSeriesMerger()).Select(
 		false,
 		nil,
 		labels.MustNewMatcher(labels.MatchEqual, "__name__", "up"),
+		labels.MustNewMatcher(labels.MatchEqual, "pod", "prometheus-server-97cfb8984-4vljm"),
 	)
 
 	for ss.Next() {
 		series := ss.At()
 		lbs := series.Labels()
 		it := series.Iterator(nil)
-		for it.Next() == chunkenc.ValFloat {
-			ts, val := it.At()
-			fmt.Printf("%s %g %d\n", lbs, val, ts)
+
+		for it.Next() {
+			chk := it.At().Chunk
+
+			if chk.Encoding().String() != "XOR" {
+				return
+			} else {
+				it := chk.Iterator(nil)
+				for it.Next() != chunkenc.ValNone {
+					ts, val := it.At()
+					fmt.Printf("%s %g %d\n", lbs, val, ts)
+				}
+			}
+
 		}
+
 		if it.Err() != nil {
 			t.Fatal(err)
 		}
