5924a403622882b518498af6dad7057737220c84
Resurgence72 <wuhaoyang1105@qq.com>
2024-01-10 18:08:26 +0800

    Update read.go

--- pkg/prometheus/read.go
+++ pkg/prometheus/read.go
@@ -10,6 +10,6 @@ func (p *Prometheus) RemoteRead(
 	span *pb.DurationSpan,
 	interval time.Duration,
 	matchers ...pb.Matcher,
-) ([]pb.TimeSeries, int64, error) {
+) (Iterator, error) {
 	return p.remoteReadV2(span, interval, matchers...)
 }
